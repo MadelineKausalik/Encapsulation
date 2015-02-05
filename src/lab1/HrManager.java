b1a8212550be8c980d5bc259d64b5791544cc2e3
mkausalik3 <mkausalik3@L11704042767.wctc.msft>
2015-02-05 15:08:51 -0600

    Finished

--- src/lab1/HrManager.java
+++ src/lab1/HrManager.java
@@ -1,9 +1,22 @@
 package lab1;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class HrManager {
+
+    private Employee employee = new Employee();
     
-    
-    
-    
+    public void hire(String firstName, String lastName, String ssn){
+
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setSsn(ssn);
+        
+        employee.becomeEmployee();
+
+        System.out.println("The employee's status is: " + employee.getStatus());
+    }
+        
+
 }
